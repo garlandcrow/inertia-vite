afad97bb03ff893bef869944d6786a130a949d6c
garlandcrow <garlandcrow00@gmail.com>
2021-07-12 09:48:12 +0900

    swap vue for svelte in config

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,39 @@
 import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+// import vue from "@vitejs/plugin-vue";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+
 const { resolve } = require("path");
 
 export default defineConfig({
-  plugins: [vue()],
-  root: "resources",
-  base: `/dist/`,
-  resolve: {
-    // Set the '@' alias so Vite can find the Breeze component imports.
-    alias: {
-      "@": "/js",
+    plugins: [svelte()],
+    root: "resources",
+    base: `/dist/`,
+    resolve: {
+        // Set the '@' alias so Vite can find the Breeze component imports.
+        alias: {
+            "@": "/js",
+        },
+        // Need to add the ".vue" extension. This is not recommended by Vite
+        // (https://vitejs.dev/config/#resolve-extensions) but necessary until
+        // the Breeze component imports are updated with .vue.
+        extensions: [".svelte", ".js", ".json", ".jsx", ".ts", ".tsx"],
     },
-    // Need to add the ".vue" extension. This is not recommended by Vite
-    // (https://vitejs.dev/config/#resolve-extensions) but necessary until
-    // the Breeze component imports are updated with .vue.
-    extensions: [".vue", ".js", ".json", ".jsx", ".ts", ".tsx"],
-  },
-  build: {
-    sourcemap: true,
-    outDir: resolve(__dirname, "public/dist"),
-    emptyOutDir: true,
-    manifest: true,
-    rollupOptions: {
-      input: "./resources/js/app.js",
+    build: {
+        sourcemap: true,
+        outDir: resolve(__dirname, "public/dist"),
+        emptyOutDir: true,
+        manifest: true,
+        rollupOptions: {
+            input: "./resources/js/app.js",
+        },
     },
-  },
-  optimizeDeps: {
-    include: [
-      "vue",
-      "@inertiajs/inertia",
-      "@inertiajs/inertia-vue3",
-      "@inertiajs/progress",
-      "axios",
-    ],
-  },
+    optimizeDeps: {
+        include: [
+            "svelte",
+            "@inertiajs/inertia",
+            "@inertiajs/inertia-svelte",
+            "@inertiajs/progress",
+            "axios",
+        ],
+    },
 });
