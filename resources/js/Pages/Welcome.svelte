b18268e8b9b3b8d6e099c60275d77758ba9eac71
garlandcrow <garlandcrow00@gmail.com>
2021-07-12 09:48:27 +0900

    update app.js for svelte and add test components

---
+++ resources/js/Pages/Welcome.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { page } from "@inertiajs/inertia-svelte";
+
+    import TestDipsatch from "../Components/TestDipsatch.svelte";
+
+    $: console.log("pageProps", $page);
+</script>
+
+<h1 class="font-bold text-2xl">Welcome Page Component</h1>
+
+<TestDipsatch
+    on:testDispatch={({ detail }) => console.log("dispatched: ", detail)}
+/>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,13 @@
 import "vite/dynamic-import-polyfill";
-import { createApp, h } from "vue";
-import {
-  App as InertiaApp,
-  plugin as InertiaPlugin,
-} from "@inertiajs/inertia-vue3";
+
+// import { createApp, h } from "vue";
+// import {
+//     App as InertiaApp,
+//     plugin as InertiaPlugin,
+// } from "@inertiajs/inertia-vue3";
+
+import { InertiaApp } from "@inertiajs/inertia-svelte";
+
 import { InertiaProgress } from "@inertiajs/progress";
 
 // Import Axios and set config here instead of bootstrap file.
@@ -14,31 +18,48 @@ import "../css/app.css";
 
 axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
-const el = document.getElementById("app");
+const app = document.getElementById("app");
 
-// Get all the page components. This is required for the Rollup production
-// build.
-const pages = import.meta.glob("./Pages/**/*.vue");
+// Get all the page components. This is required for the Rollup production build.
+const pages = import.meta.glob("./Pages/**/*.svelte");
 
-createApp({
-  render: () =>
-    h(InertiaApp, {
-      initialPage: JSON.parse(el.dataset.page),
-      // Update the resolve component to use the import syntax. The path should
-      // begin with a './' instead of using the alias.
-      // https://github.com/rollup/plugins/tree/master/packages/dynamic-import-vars#limitations
-      resolveComponent: (name) => {
-        const page = pages[`./Pages/${name}.vue`];
-        if (!page) {
-          throw new Error(`Page ${name} not found.`);
-        }
+if (app) {
+    new InertiaApp({
+        target: app,
+        props: {
+            initialPage: JSON.parse(app.dataset.page),
+            resolveComponent: (name) => {
+                const page = pages[`./Pages/${name}.svelte`];
 
-        return page().then((module) => module.default);
-      },
-    }),
-})
-  .mixin({ methods: { route } })
-  .use(InertiaPlugin)
-  .mount(el);
+                if (!page) {
+                    throw new Error(`Page ${name} not found.`);
+                }
+
+                return page().then((module) => module);
+            },
+        },
+    });
+}
+
+// createApp({
+//     render: () =>
+//         h(InertiaApp, {
+//             initialPage: JSON.parse(el.dataset.page),
+//             // Update the resolve component to use the import syntax. The path should
+//             // begin with a './' instead of using the alias.
+//             // https://github.com/rollup/plugins/tree/master/packages/dynamic-import-vars#limitations
+//             resolveComponent: (name) => {
+//                 const page = sveltePages[`./Pages/${name}.svelte`];
+//                 if (!page) {
+//                     throw new Error(`Page ${name} not found.`);
+//                 }
+
+//                 return page().then((module) => module.default);
+//             },
+//         }),
+// })
+//     .mixin({ methods: { route } })
+//     .use(InertiaPlugin)
+//     .mount(el);
 
 InertiaProgress.init({ color: "#4B5563" });
